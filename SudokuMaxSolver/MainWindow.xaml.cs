c9c98a8bcea38c626cb56a1f931e40c2d7a1d0db
Maksymilian Hebda <maksymilian.hebda@gmail.com>
2020-08-13 18:25:17 +0200

    add first skeleton popup

--- SudokuMaxSolver/MainWindow.xaml.cs
+++ SudokuMaxSolver/MainWindow.xaml.cs
@@ -22,7 +22,9 @@ namespace SudokuMaxSolver
     public partial class MainWindow : Window
     {
         BoardTab board = new BoardTab();
-        Button[,] buttonMain = new Button[9, 9];
+        Button[,] buttonMain = new Button[9, 9];        //main board
+        Popup popupMain = new Popup();
+        Button[] buttonPopup = new Button[9];           //small board popup
 
         public MainWindow()
         {
@@ -54,15 +56,52 @@ namespace SudokuMaxSolver
             }
             this.Height = dockPanelMain.Height;
         }
+        void generatePopup(byte yMain, byte xMain)
+        {
+            UniformGrid grid = new UniformGrid();
+            grid.Columns = 3;
+            grid.Width = 100;
+            grid.Height = 100;
+            for (byte iPopup = 0; iPopup < 9; iPopup++)
+            {
+                buttonPopup[iPopup] = new Button();
+                buttonPopup[iPopup].Content = iPopup + 1;
+                buttonPopup[iPopup].Width = 30;
+                buttonPopup[iPopup].Height = 30;
+                buttonPopup[iPopup].Name = "p" + (iPopup + 1);
+                buttonPopup[iPopup].Click += bPopup_Click;
+                grid.Children.Add(buttonPopup[iPopup]);
+            }
+
+            popupMain.Child = grid;
+            popupMain.PlacementTarget = (Button)buttonMain[yMain,xMain];
+            popupMain.Placement = PlacementMode.Right;
+            popupMain.Width = 102;
+            popupMain.Height = 102;
+            popupMain.VerticalOffset = -10;
+            popupMain.HorizontalOffset = -10;
+            popupMain.IsOpen = true;
+        }
+
+        private void bPopup_Click(object sender, RoutedEventArgs e)
+        {
+            // hidden popup
+            popupMain.IsOpen = false;
+        }
 
         private void bMain_Click(object sender, RoutedEventArgs e)
         {
+            // close old popup if is open
+            if (popupMain.IsOpen == true) popupMain.IsOpen = false;
+
             // number y and x of our button
             byte y = byte.Parse(((Button)sender).Name[1] + "");
-            byte x =byte.Parse(((Button)sender).Name[2] + "");
+            byte x = byte.Parse(((Button)sender).Name[2] + "");
             board.set(y, x, x);
             buttonMain[y, x].Content = "a";
             Console.WriteLine(board.get(y, x));
+
+            generatePopup(y, x);    //show the popup after clicking the button
         }
     }
 }
